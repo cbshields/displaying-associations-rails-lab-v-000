cc68da68cf76401e71ffb903ac4f20cc56a90159
Caroline Shields <cbshields@outlook.com>
2018-03-24 20:37:15 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,10 +43,6 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
-  def artist_name
-    song = Song.find(params[:id])
-    song.artist.name
-  end
 
   private
 
